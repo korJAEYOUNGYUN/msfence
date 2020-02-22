6be97f655be87fe21143078557528d7d3ac45c09
JAEYOUNGYUN <48082827+korJAEYOUNGYUN@users.noreply.github.com>
2020-02-22 23:46:28 +0900

    디자인 수정중

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
-$('a[class="nav-item nav-link menu-item"]')
-    .not('[carousel-control-prev]')
-    .click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-}, 500);
-    return false;
-});
+// $('a[class="nav-item nav-link menu-item"]')
+//     .not('[carousel-control-prev]')
+//     .click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top
+// }, 500);
+//     return false;
+// });
 
